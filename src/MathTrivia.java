3e03779d25f7732e9a8a4a6febbb8f1cce9ab663
rozalenadous <rma9547@nyu.edu>
2022-11-30 21:08:15 -0500

    Fixed Project

--- src/MathTrivia.java
+++ src/MathTrivia.java
@@ -3,15 +3,12 @@ public class MathTrivia {
     private double num2;
     private String operation;
 
-    public MathTrivia(String difference, int num1, int num2){
-        operation = difference;
-        this.num1= num1;
-        this.num2 = num2;
+    public MathTrivia(){
+      // make a get random operation here
     }
-    public MathTrivia(String exponential, double num1, int num2){
-        operation = exponential;
-        this.num1= num1;
-        this.num2 = num2;
+    public MathTrivia(String operation){
+        this.operation = "exponential";
+
     }
     public MathTrivia(String division, double num1, double num2){
         operation = division;
@@ -23,25 +20,31 @@ public class MathTrivia {
         double myNum = (int) (Math.random()*100) +1;
         return myNum;
     }
-    public double MathDivision(){
+    public double mathSolve(double num1, double num2){
+       if (operation.equals("division")){
+           return num1/num2;
+       } else if (operation.equals("exponents")){
+           return Math.pow(num1,num2);
+       } else {
+           return num1 - num2;
+       }
     }
     public String toString(){
-        return "The answer to this problem is " + 5; // stand in int
+        return "The answer to this problem is " + mathSolve(num1, num2); // stand in int
 
     }
-    public void getOperation(String str){
-        if (str.contains("difference") ){
-            System.out.print("difference");
-        }
-        if (str.contains("power") ){
-            System.out.print("exponential");
-        }
-        if (str.contains("divided") ){
-            System.out.print("division");
-        }
-        else {
-            System.out.print("This is not a valid operation");
+    public String getRandOperation(){
+        int num1 = (int) (Math.random()*2) + 1;
+        if (num1 == 1) {
+            return "division";
+        } else if (num1 ==2){
+            return "exponents";
+        } else {
+            return "subtraction";
         }
     }
-
+// add a void method called getscore
+    public void getScore(int score){
+        System.out.println("Your score is now " + score);
+    }
 }
--- src/MathTriviaRunner.java
+++ src/MathTriviaRunner.java
@@ -2,6 +2,37 @@ import java.util.Scanner;
 public class MathTriviaRunner {
     public static void main(String [] args){
  Scanner scan = new Scanner(System.in);
- System.out.println("This is a math trivia game, ");
+ System.out.println("This is a math trivia game \n " +
+         "You may choose to practice exponents, division, or subtraction, or you can choose a random operation " +
+         "\n Please state which you'd like to practice: ");
+  String myOp = scan.nextLine();
+ while(!(myOp.equals("division") || myOp.equals("exponents") || myOp.equals("substraction"))){
+    System.out.println("This is not a valid answer.");
+    myOp = scan.nextLine();
+ }
+
+ int score = 0;
+ // do when score = 4 return "your score is at 4! that's so close
+while (score != 5){
+    // repeat 3 more times for subtraction, exponents, and random
+    if (myOp.equals("division")){
+        MathTrivia divPractice = new MathTrivia("division");
+        double num1 = divPractice.getRandom();
+        double num2 = divPractice.getRandom();
+        System.out.print("What is " + num1 + " divided by " + num2 + "? \n Please round to the nearest hundreth");
+        double divAnswer = scan.nextDouble();
+        if (divAnswer == divPractice.mathSolve(num1,num2)){
+            System.out.println("That's correct!");
+            score ++;
+            divPractice.getScore(score);
+        } else {
+           System.out.println(divPractice.toString()) ;
+        }
+
+
+    }
+}
+
+
     }
 }
