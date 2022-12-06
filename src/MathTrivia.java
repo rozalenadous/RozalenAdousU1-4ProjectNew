6db0d84aa44797befdf715d787888ab887971001
rozalenadous <rma9547@nyu.edu>
2022-12-06 08:59:41 -0500

    Final Project

--- src/MathTrivia.java
+++ src/MathTrivia.java
@@ -1,49 +1,88 @@
+/** this is a class that represents a math operation
+ *
+ */
 public class MathTrivia {
-    private double num1;
-    private double num2;
+    /** this is the math operation */
     private String operation;
 
+    /** this zero-parameter constructor initializes operation to a random operation using the getRandOperation method*/
     public MathTrivia(){
-      // make a get random operation here
         operation = getRandOperation();
     }
+
+    /** single parameter constructor that sets operation to the user operation */
     public MathTrivia(String operation){
         this.operation = operation;
-
     }
-    // how the user gets num1 and num2
-    public double getRandom(){
-        double myNum = (int) (Math.random()*10) +1;
+
+    /** get random double */
+    public double getRandomNum(){
+        double myNum = (int) (Math.random()*20) +1;
         return myNum;
     }
+
+    /** solve equation based on operation
+     * @param num1 - the first random number
+     * @param num2 - the second random number
+     * @return the solution to the equation as a double
+     */
     public double mathSolve(double num1, double num2){
-       if (operation.equals("division")){
-           return num1/num2;
-       } else {
-           return (int) num1 - (int) num2;
-       }
-    }
-   // public String toString(){
-    //    return "The answer to this problem is " + mathSolve(num1, num2); // stand in int
-    //}
-    public String toString(double num1, double num2){
         if (operation.equals("division")){
-            return "What is " + num1 + " divided by " + num2 + "? \n Please round to the nearest hundreth";
+            double dividedNums = num1/num2;
+            return roundedToHundredth(dividedNums);
         } else {
-            return "What is " + (int) num2 + " subtracted from " + (int) num1 + "?";
+            return num1 - num2;
         }
     }
-    // only ever returns division??
+
+    /** finds and returns user's math operation
+     * @return operation as a string
+     */
+    public String toString(){
+        if (operation.equals("division")){
+            return "division";
+        } else{
+            return "subtraction";
+        }
+    }
+
+    /** converts the equation to a verbal problem using toString method
+     * @param num1 - user's first random number
+     * @param num2 - user's second random number
+     * @return the equation as a word problem
+     */
+    public String toString(double num1, double num2){
+        if (toString().equals("division")){
+            return "What is " + num1 + " divided by " + num2 + "? \nPlease round to the nearest hundreth";
+        } else {
+            return "What is " + (int) num2 + " subtracted from " + (int) num1 + "? \nPlease provide a double.";
+        }
+    }
+
+    /** choose a random operation using the Math.random() operator
+     * @return a random math operation
+     */
     public String getRandOperation(){
-        int num1 = (int) (Math.random() * 2) + 1;
-        if (num1 == 1) {
+        int randNum = (int) (Math.random() * 2) + 1;
+        if (randNum == 1) {
             return "subtraction";
         } else {
             return "division";
         }
     }
-// add a void method called getscore
+
+    /** prints out the user's score
+     * @param score - the user's current score
+     */
     public void getScore(int score){
         System.out.println("Your score is now " + score);
+    }
+
+    /** rounds the number to the nearest hundredth
+     * @param toRound - number to be rounded
+     * @return a double rounded to the nearest hundredth
+     */
+    public double roundedToHundredth(double toRound) {
+        return (double) Math.round(toRound * 100) / 100;
     }
 }
--- src/MathTriviaRunner.java
+++ src/MathTriviaRunner.java
@@ -1,65 +1,66 @@
 import java.util.Scanner;
 public class MathTriviaRunner {
     public static void main(String [] args){
- Scanner scan = new Scanner(System.in);
- System.out.print("This is a math trivia game \n " +
-         "You may choose to practice division or subtraction, or you can choose a random operation " +
-         "\n Please state which you'd like to practice: ");
-  String myOp = scan.nextLine();
- while(!(myOp.equals("division") || myOp.equals("random") || myOp.equals("subtraction"))){
-    System.out.println("This is not a valid answer.");
-    myOp = scan.nextLine();
- }
+        Scanner scan = new Scanner(System.in);
 
- int score = 0;
- // do when score = 4 return "your score is at 4! that's so close
-while (score != 5){
-    // repeat 3 more times for subtraction, exponents, and random
+        // introduces the game and allows a user to input their operation
+        System.out.print("This is a math trivia game\n" +
+                "You may choose to practice division or subtraction, or you can choose a random operation" +
+                "\nPlease state which you'd like to practice: \n");
+        String myOperation = scan.nextLine();
 
-    // Maybe instead of all this logic in the main method make it a method
-    if (myOp.equals("division")){
-        MathTrivia divPractice = new MathTrivia("division");
-        double num1 = divPractice.getRandom();
-        double num2 = divPractice.getRandom();
-        System.out.print(divPractice.toString(num1, num2));
-        double divAnswer = scan.nextDouble();
-        if (divAnswer == divPractice.mathSolve(num1,num2)){
-            System.out.println("That's correct!");
-            score ++;
-            divPractice.getScore(score);
-        } else {
-           System.out.println("That's wrong. The correct answer is "  + divPractice.mathSolve(num1,num2) ) ;
+        // asks the user to enter an operation till it is valid (random, division, or subtraction)
+        while(!(myOperation.equals("division") || myOperation.equals("random") || myOperation.equals("subtraction"))){
+            System.out.println("This is not a valid answer.");
+            myOperation = scan.nextLine();
         }
-    }
-    if (myOp.equals("subtraction")){
-        MathTrivia subPractice = new MathTrivia("subtraction");
-        double num1 = subPractice.getRandom();
-        double num2 = subPractice.getRandom();
-        System.out.print(subPractice.toString(num1, num2));
-        double subAnswer = scan.nextDouble();
-        if (subAnswer == subPractice.mathSolve(num1,num2)){
-            System.out.println("That's correct!");
-            score ++;
-            subPractice.getScore(score);
-        } else {
-            System.out.println("That's wrong. The correct answer is "  + subPractice.mathSolve(num1,num2) ) ;
+
+        // initializes score to 0 and continues the game until the score is 5
+        int score = 0;
+        while (score != 5){
+            if (myOperation.equals("division")){ //
+                MathTrivia divPractice = new MathTrivia("division");
+                double num1 = divPractice.getRandomNum();
+                double num2 = divPractice.getRandomNum();
+                System.out.print(divPractice.toString(num1, num2));
+                double divAnswer = scan.nextDouble();
+                if (divAnswer == divPractice.mathSolve(num1,num2)){
+                    System.out.println("That's correct!");
+                    score ++;
+                    divPractice.getScore(score);
+                } else {
+                    System.out.println("That's wrong. The correct answer is "  + divPractice.mathSolve(num1,num2) ) ;
+                }
+            }
+            if (myOperation.equals("subtraction")){
+                MathTrivia subPractice = new MathTrivia("subtraction");
+                double num1 = subPractice.getRandomNum();
+                double num2 = subPractice.getRandomNum();
+                System.out.print(subPractice.toString(num1, num2));
+                double subAnswer = scan.nextDouble();
+                if (subAnswer == subPractice.mathSolve(num1,num2)){
+                    System.out.println("That's correct!");
+                    score ++;
+                    subPractice.getScore(score);
+                } else {
+                    System.out.println("That's wrong. The correct answer is "  + subPractice.mathSolve(num1,num2) ) ;
+                }
+            }
+            if (myOperation.equals("random")){
+                MathTrivia randPractice = new MathTrivia();
+                double num1 = randPractice.getRandomNum();
+                double num2 = randPractice.getRandomNum();
+                System.out.print(randPractice.toString(num1, num2));
+                double randAnswer = scan.nextDouble();
+                if (randAnswer == randPractice.mathSolve(num1,num2)){
+                    System.out.println("That's correct!");
+                    score ++;
+                    randPractice.getScore(score);
+                } else {
+                    System.out.println("That's wrong. The correct answer is "  + randPractice.mathSolve(num1,num2) ) ;
+                }
+            }
         }
-    }
-    if (myOp.equals("random")){
-        MathTrivia randPractice = new MathTrivia();
-        double num1 = randPractice.getRandom();
-        double num2 = randPractice.getRandom();
-        System.out.print(randPractice.toString(num1, num2));
-        double randAnswer = scan.nextDouble();
-        if (randAnswer == randPractice.mathSolve(num1,num2)){
-            System.out.println("That's correct!");
-            score ++;
-            randPractice.getScore(score);
-        } else {
-            System.out.println("That's wrong. The correct answer is "  + randPractice.mathSolve(num1,num2) ) ;
-        }
-    }
-}
-System.out.println("Congrats! You passed the trivia! Come back if you want more math practice!");
+        System.out.println("Congrats! You passed the trivia! Come back if you want more math practice!");
     }
 }
